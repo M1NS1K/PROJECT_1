e387dea5f495cb5a9f85bcec64271ba5171f7247
M1NS1K <rlaalstlr2001@gmail.com>
2024-04-12 02:28:45 +0900

    feat: filterChain 구현

--- src/main/java/TEST/backend/article/config/SecurityConfig.java
+++ src/main/java/TEST/backend/article/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package TEST.backend.article.config;
 
 import java.util.Arrays;
+
+import TEST.backend.article.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +19,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
@@ -23,19 +28,33 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            CustomAuthenticationFilter customAuthenticationFilter,
+            JwtAuthorizationFilter jwtAuthorizationFilter
+    ) throws Exception {
+        log.debug("[+] WebSecurityConfig Start !!! ");
         http.csrf(AbstractHttpConfigurer::disable);
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource))
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 
         http
                 .authorizeHttpRequests(authorize -> authorize
@@ -45,15 +64,14 @@ public class SecurityConfig {
                 .requestMatchers("/error.html").permitAll()
                 .anyRequest().authenticated());
 
-        http.addFilterBefore()
-
-
+        http.addFilterBefore(jwtAuthorizationFilter, BasicAuthenticationFilter.class);
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.formLogin(login -> login
                 .loginPage("/login")
                 .successHandler(new SimpleUrlAuthenticationSuccessHandler("/main/rootPage"))
                 .permitAll());
 
+        http.addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
 //        http
@@ -88,7 +106,77 @@ public class SecurityConfig {
     }
 
     @Bean
-    public J
+    public CustomAuthenticationFilter customAuthenticationFilter(
+        AuthenticationManager authenticationManager,
+        CustomAuthSuccessHandler customAuthSuccessHandler,
+        CustomAuthFailureHandler customAuthFailureHandler
+    ) {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter()
+    }
+
+    /**
+     * 2. authenticate 의 인증 메서드를 제공하는 매니져로'Provider'의 인터페이스를 의미한다.
+     * 이 메서드는 인증 매니저를 생성한다. 인증 매니저는 인증 과정을 처리하는 역할을 한다.
+     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(CustomAuthenticationProvider customAuthenticationProvider) {
+        return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
+    }
+
+    /**
+     * 3. '인증' 제공자로 사용자의 이름과 비밀번호가 요구된다.
+     * 이 메서드는 사용자 정의 인증 제공자를 생성한다. 인증 제공자는 사용자 이름과 비밀번호를 사용하여 인증을 수행한다.
+     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
+     */
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider(UserDetailsService userDetailsService) {
+        return new CustomAuthenticationProvider(
+            userDetailsService
+        );
+    }
+
+    /**
+     * 4. Spring Security 기반의 사용자의 정보가 맞을 경우 수행이 되며 결과값을 리턴해주는 Handler
+     * customLoginSuccessHandler: 이 메서드는 인증 성공 핸들러를 생성한다. 인증 성공 핸들러는 인증 성공시 수행할 작업을 정의한다.
+     */
+    @Bean
+    public CustomAuthSuccessHandler customLoginSuccessHandler() {
+        return new CustomAuthSuccessHandler();
+    }
+
+    /**
+     * 5. Spring Security 기반의 사용자의 정보가 맞지 않을 경우 수행이 되며 결과값을 리턴해주는 Handler
+     * customLoginFailureHandler: 이 메서드는 인증 실패 핸들러를 생성한다. 인증 실패 핸들러는 인증 실패시 수행할 작업을 정의한다.
+     */
+    @Bean
+    public CustomAuthFailureHandler customLoginFailureHandler() {
+        return new CustomAuthFailureHandler();
+    }
+
+    /**
+     * "JWT 토큰을 통하여서 사용자를 인증한다." -> 이 메서드는 JWT 인증 필터를 생성한다.
+     * JWT 인증 필터는 요청 헤더의 JWT 토큰을 검증하고, 토큰이 유효하면 토큰에서 사용자의 정보와 권한을 추출하여 SecurityContext에 저장한다.
+     */
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter(CustomUserDetailsService userDetailsService) {
+        return new JwtAuthorizationFilter(userDetailsService);
+    }
+
+    /**
+     * isAdmin 메소드는 Supplier<Authentication>와 RequestAuthorizationContext를 인자로 받아서 "ADMIN" 역할을 가진 사용자인지 확인한다.
+     * 만약 사용자가 "ADMIN" 역할을 가지고 있다면, AuthorizationDecision 객체는 true를 반환하고, 그렇지 않다면 false를 반환한다.
+     */
+    private AuthorizationDecision isAdmin(
+        Supplier<Authentication> authenticationSupplier,
+        RequestAuthorizationContext requestAuthorizationContext
+    ) {
+        return new AuthorizationDecision(
+            authenticationSupplier.get()
+                .getAuthorities()
+                .contains(new SimpleGrantedAuthority("ADMIN"))
+        );
+    }
 
     @Bean
     PasswordEncoder passwordENcoder() {
