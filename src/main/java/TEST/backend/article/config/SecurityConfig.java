55c60c903fbbffd04344313c0f32ddd4d039a506
M1NS1K <rlaalstlr2001@gmail.com>
2024-04-27 09:50:36 +0900

    refactor: SecurityConfig refactoring

--- src/main/java/TEST/backend/article/config/SecurityConfig.java
+++ src/main/java/TEST/backend/article/config/SecurityConfig.java
@@ -1,38 +1,15 @@
 package TEST.backend.article.config;
 
-import java.util.Arrays;
-
-import TEST.backend.article.filter.JwtAuthorizationFilter;
+import TEST.backend.article.constant.RoleType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 
 @Slf4j
 @Configuration
@@ -41,150 +18,37 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring()
-            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(
-            HttpSecurity http,
-            CustomAuthenticationFilter customAuthenticationFilter,
-            JwtAuthorizationFilter jwtAuthorizationFilter
-    ) throws Exception {
-        log.debug("[+] WebSecurityConfig Start !!! ");
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/resources/**", "/static/**").permitAll()
-                .requestMatchers("/css/**").permitAll()
-                .requestMatchers("/main/rootPage").permitAll()
-                .requestMatchers("/error.html").permitAll()
-                .anyRequest().authenticated());
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole(RoleType.ADMIN.name())
+                        .requestMatchers("/my/**").hasAnyRole(RoleType.ADMIN.name(), RoleType.USER.name())
+                        .anyRequest().authenticated()
+                );
 
-        http.addFilterBefore(jwtAuthorizationFilter, BasicAuthenticationFilter.class);
-        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http.formLogin(login -> login
-                .loginPage("/login")
-                .successHandler(new SimpleUrlAuthenticationSuccessHandler("/main/rootPage"))
-                .permitAll());
+        http
+                .formLogin((auth) -> auth.loginPage("/login")
+                        .loginProcessingUrl("/loginProcess")
+                        .permitAll()
+                );
 
-        http.addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http
+                .logout((auth) -> auth
+                        .logoutUrl("/logout")
+                );
+
+        http
+                .csrf((auth) -> auth.disable());
+
         return http.build();
-
-//        http
-//                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-//                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
-//                .csrf((csrf) -> csrf
-//                        .ignoringRequestMatchers(new AntPathRequestMatcher("/")))
-//                .headers((headers) -> headers
-//                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
-//                                XFrameOptionsMode.SAMEORIGIN)))
-//                .formLogin((formLogin) -> formLogin
-//                        .loginPage("/user/login")
-//                        .defaultSuccessUrl("/"))
-//                .logout((logout) -> logout
-//                        .logoutRequestMatcher(new AntPathRequestMatcher("/"))
-//                        .logoutSuccessUrl("/")
-//                        .invalidateHttpSession(true));
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 출처(origin)에 대해 허용
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용된 HTTP 메서드를 설정
-        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Content-Type", "Authorization", "X-XSRF-token")); // 허용된 요청 헤더를 설정
-        configuration.setAllowCredentials(false); // 크로스 도메인 요청에서 쿠키 등의 인증 정보를 허용할지 여부를 설정
-        configuration.setMaxAge(3600L); // 브라우저에서 preflight 요청 결과를 캐시할 시간을 초 단위로 설정
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(); // 브라우저에서 preflight 요청 결과를 캐시할 시간을 초 단위로 설정
-        source.registerCorsConfiguration("/**", configuration);  // 모든 URL 패턴(/**)에 대해 앞서 생성한 CORS 설정을 등록
-        return source;
-    }
-
-    @Bean
-    public CustomAuthenticationFilter customAuthenticationFilter(
-        AuthenticationManager authenticationManager,
-        CustomAuthSuccessHandler customAuthSuccessHandler,
-        CustomAuthFailureHandler customAuthFailureHandler
-    ) {
-        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter()
-    }
-
-    /**
-     * 2. authenticate 의 인증 메서드를 제공하는 매니져로'Provider'의 인터페이스를 의미한다.
-     * 이 메서드는 인증 매니저를 생성한다. 인증 매니저는 인증 과정을 처리하는 역할을 한다.
-     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
-     */
-    @Bean
-    public AuthenticationManager authenticationManager(CustomAuthenticationProvider customAuthenticationProvider) {
-        return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
-    }
-
-    /**
-     * 3. '인증' 제공자로 사용자의 이름과 비밀번호가 요구된다.
-     * 이 메서드는 사용자 정의 인증 제공자를 생성한다. 인증 제공자는 사용자 이름과 비밀번호를 사용하여 인증을 수행한다.
-     * 과정: CustomAuthenticationFilter → AuthenticationManager(interface) → CustomAuthenticationProvider(implements)
-     */
-    @Bean
-    public CustomAuthenticationProvider customAuthenticationProvider(UserDetailsService userDetailsService) {
-        return new CustomAuthenticationProvider(
-            userDetailsService
-        );
-    }
-
-    /**
-     * 4. Spring Security 기반의 사용자의 정보가 맞을 경우 수행이 되며 결과값을 리턴해주는 Handler
-     * customLoginSuccessHandler: 이 메서드는 인증 성공 핸들러를 생성한다. 인증 성공 핸들러는 인증 성공시 수행할 작업을 정의한다.
-     */
-    @Bean
-    public CustomAuthSuccessHandler customLoginSuccessHandler() {
-        return new CustomAuthSuccessHandler();
-    }
-
-    /**
-     * 5. Spring Security 기반의 사용자의 정보가 맞지 않을 경우 수행이 되며 결과값을 리턴해주는 Handler
-     * customLoginFailureHandler: 이 메서드는 인증 실패 핸들러를 생성한다. 인증 실패 핸들러는 인증 실패시 수행할 작업을 정의한다.
-     */
-    @Bean
-    public CustomAuthFailureHandler customLoginFailureHandler() {
-        return new CustomAuthFailureHandler();
-    }
-
-    /**
-     * "JWT 토큰을 통하여서 사용자를 인증한다." -> 이 메서드는 JWT 인증 필터를 생성한다.
-     * JWT 인증 필터는 요청 헤더의 JWT 토큰을 검증하고, 토큰이 유효하면 토큰에서 사용자의 정보와 권한을 추출하여 SecurityContext에 저장한다.
-     */
-    @Bean
-    public JwtAuthorizationFilter jwtAuthorizationFilter(CustomUserDetailsService userDetailsService) {
-        return new JwtAuthorizationFilter(userDetailsService);
-    }
-
-    /**
-     * isAdmin 메소드는 Supplier<Authentication>와 RequestAuthorizationContext를 인자로 받아서 "ADMIN" 역할을 가진 사용자인지 확인한다.
-     * 만약 사용자가 "ADMIN" 역할을 가지고 있다면, AuthorizationDecision 객체는 true를 반환하고, 그렇지 않다면 false를 반환한다.
-     */
-    private AuthorizationDecision isAdmin(
-        Supplier<Authentication> authenticationSupplier,
-        RequestAuthorizationContext requestAuthorizationContext
-    ) {
-        return new AuthorizationDecision(
-            authenticationSupplier.get()
-                .getAuthorities()
-                .contains(new SimpleGrantedAuthority("ADMIN"))
-        );
-    }
-
-    @Bean
-    PasswordEncoder passwordENcoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+
 }
