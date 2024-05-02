1363075ea4f2fca47d3ccc7a174a2a74e69eb42f
M1NS1K <rlaalstlr2001@gmail.com>
2024-05-02 18:41:04 +0900

    refactor: TokenProvider

--- src/main/java/TEST/backend/article/utils/TokenProvider.java
+++ src/main/java/TEST/backend/article/utils/TokenProvider.java
@@ -1,5 +1,7 @@
 package TEST.backend.article.utils;
 
+import static com.nimbusds.oauth2.sdk.token.DPoPTokenError.INVALID_TOKEN;
+
 import TEST.backend.article.domain.dto.UserInfo;
 import TEST.backend.article.domain.entity.User;
 import io.jsonwebtoken.Claims;
@@ -7,22 +9,30 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import javax.crypto.SecretKey;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.core.token.Token;
 import org.springframework.security.core.token.TokenService;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.util.StringUtils;
 
 @Slf4j
 @Component
@@ -46,6 +56,11 @@ public class TokenProvider {
         return generateToken(authentication, ACCESS_TOKEN_EXPIRE_TIME);
     }
 
+    public void generateRefreshToken(Authentication authentication, String accessToken) {
+        String refreshToken = generateToken(authentication, REFRESH_TOKEN_EXPIRE_TIME);
+        // token save or update
+    }
+
     private String generateToken(Authentication authentication, long expireTime) {
         Date now = new Date();
         Date expiredDate = new Date(now.getTime() + expireTime);
@@ -61,6 +76,57 @@ public class TokenProvider {
                 .setExpiration(expiredDate)
                 .signWith(secretKey, SignatureAlgorithm.HS512)
                 .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+        List<SimpleGrantedAuthority> authorities = getAuthentication(claims);
+
+        User principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
+    }
+
+    private List<SimpleGrantedAuthority> getAuthorities(Claims claims) {
+        return Collections.singletonList(new SimpleGrantedAuthority(
+                claims.get(KEY_ROLE).toString()
+        ));
+    }
+
+    private String reissueAccessToken(String accessToken) {
+        if(StringUtils.hasText(accessToken)) {
+            Token token = tokenService.findByAccessTokenOrThrow(accessToken);
+            String refreshToken = token.getRefreshToken();
+
+            if(validateToken(refreshToken)) {
+                String reissueAccessToken = generateAccessToken(getAuthentication(refreshToken));
+                tokenService.updateToken(reissueAccessToken, token);
+                return reissueAccessToken;
+            }
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        if(!StringUtils.hasText(token)) {
+            return false;
+        }
+
+        Claims claims = parseClaims(token);
+        return claims.getExpiration().after(new Date());
+    }
+
+    public Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().verifyWith(secretKey).build()
+                    .parseSignedClaims(token).getPayload();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        } catch (MalformedJwtException e) {
+            throw new TokenException(INVALID_TOKEN);
+        } catch (SecurityException e) {
+            throw new TokenException(INVALID_JWT_SIGNATURE);
+        }
+    }
     }
 
 }
