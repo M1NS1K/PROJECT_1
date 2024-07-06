e131a797743e49956a8173e7ce508a103ea85109
M1NS1K <rlaalstlr2001@gmail.com>
2024-07-06 20:04:49 +0900

    feat: FormAuthenticationSuccessHandler

--- src/main/java/TEST/backend/security/config/SecurityConfig.java
+++ src/main/java/TEST/backend/security/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package TEST.backend.security.config;
 
+import TEST.backend.security.handler.FormAuthenticationSuccessHandler;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +16,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
 
 @Slf4j
@@ -21,25 +25,30 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
-            .requestMatchers("/css/**", "images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-            .requestMatchers("/").permitAll()
-            .anyRequest().authenticated())
-		        .formLogin(form -> form.loginPage("/login").permitAll()).userDetailsService(userDetailsService)
-		        .authenticationProvider(authenticationProvider);
-        return http.build();
-    }
+	private final AuthenticationProvider authenticationProvider;
+	private final AuthenticationProvider restAuthenticationProvider;
+	private final AuthenticationProvider<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
+	private final FormAuthenticationSuccessHandler successHandler;
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
-        return new InMemoryUserDetailsManager(user);
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth
+										.requestMatchers("/css/**", "images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
+										.requestMatchers("/").permitAll()
+										.anyRequest().authenticated())
+						.formLogin(form -> form.loginPage("/login").permitAll()).userDetailsService(userDetailsService)
+						.authenticationProvider(authenticationProvider);
+		return http.build();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
