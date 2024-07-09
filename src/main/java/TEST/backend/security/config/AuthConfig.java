e1b7adc186dd7765496e5a15c939520a00f48014
M1NS1K <rlaalstlr2001@gmail.com>
2024-07-09 23:56:02 +0900

    feat: RestAuthenticationToken

--- src/main/java/TEST/backend/security/config/AuthConfig.java
+++ src/main/java/TEST/backend/security/config/AuthConfig.java
@@ -2,6 +2,7 @@ package TEST.backend.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -10,5 +11,10 @@ public class AuthConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public RoleHierarchyImpl roleHierarchy() {
+		return null;
 	}
 }
