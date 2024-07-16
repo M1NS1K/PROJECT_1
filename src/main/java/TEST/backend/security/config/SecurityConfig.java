df75366dea0ec2618f82175e21f1cb9c0bcd42ad
M1NS1K <rlaalstlr2001@gmail.com>
2024-07-16 23:13:39 +0900

    refactor: Securityconfig

--- src/main/java/TEST/backend/security/config/SecurityConfig.java
+++ src/main/java/TEST/backend/security/config/SecurityConfig.java
@@ -7,15 +7,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
@@ -27,8 +22,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 public class SecurityConfig {
 
 	private final AuthenticationProvider authenticationProvider;
-	private final AuthenticationProvider restAuthenticationProvider;
-	private final AuthenticationProvider<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
+	private final AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> authenticationDetailsSource;
 	private final FormAuthenticationSuccessHandler successHandler;
 	private final FormAuthenticationFailureHandler failureHandler;
 
